29cc40d2dec0f517142d3dbdf6cec3bd8483014b
Afshin Moussavi <142570526+AfshinMoussavi@users.noreply.github.com>
2024-04-07 05:16:41 +0330

    Create A. Watermelon.py

---
+++ codeforces/A. Watermelon.py
@@ -0,0 +1,12 @@
+# Name   :   Watermelon
+# URL    :   https://codeforces.com/problemset/problem/4/A
+# Github :   https://github.com/AfshinMoussavi
+
+number = int(input())
+
+if number == 2:
+    print('NO')
+elif number % 2 == 0:
+    print('YES')
+else:
+    print('NO')
