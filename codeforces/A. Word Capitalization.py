061bdefc80d12634e44b3577127913d4d7baf1a2
Afshin Moussavi <142570526+AfshinMoussavi@users.noreply.github.com>
2024-04-08 06:23:42 +0330

    Create A. Word Capitalization.py

---
+++ codeforces/A. Word Capitalization.py
@@ -0,0 +1,7 @@
+# Name   :   Word Capitalization
+# URL    :   https://codeforces.com/problemset/problem/281/A
+# Github :   https://github.com/AfshinMoussavi
+
+string = str(input())
+string = string[0].upper() + string[1:]
+print(string)
