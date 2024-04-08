b9e2361f3a416927198a182dd4adc68ba7cc4f65
Afshin Moussavi <142570526+AfshinMoussavi@users.noreply.github.com>
2024-04-08 06:21:56 +0330

    Create A. Dubstep.py

---
+++ codeforces/A. Dubstep.py
@@ -0,0 +1,6 @@
+# Name   :   Dubstep
+# URL    :   https://codeforces.com/problemset/problem/208/A
+# Github :   https://github.com/AfshinMoussavi
+
+string = input().replace('WUB', ' ').split()
+print(*string)
